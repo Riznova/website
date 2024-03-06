d97fdf911a59fddcffca57f209d8f2f4810b95bb
mahmoudyusof <mahmoud.yusof27@gmail.com>
2024-03-06 14:10:06 +0200

    adding firebase support and contact form

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app"
+import {getFirestore} from "firebase/firestore"
+import { getAnalytics } from "firebase/analytics";
 import DefaultLayout from '~/layouts/Default.vue'
 
 
@@ -13,10 +17,31 @@ import "~/assets/css/style.css"
 import "prismjs/themes/prism-tomorrow.css";
 
 export default function (Vue, { router, head, appOptions }) {
+
+  // Import the functions you need from the SDKs you need
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDqEIMtqEAhu56beNaYFT2FZQzpvHgev2M",
+    authDomain: "riznova-866f9.firebaseapp.com",
+    projectId: "riznova-866f9",
+    storageBucket: "riznova-866f9.appspot.com",
+    messagingSenderId: "62034778089",
+    appId: "1:62034778089:web:3bb6eb8f2411a8431d5d78",
+    measurementId: "G-1E231B8SZ9"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  Vue.prototype.$db = getFirestore(app);
+
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
-
-  console.log(appOptions)
 
   head.link.push(
     {
